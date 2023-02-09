5891ef77e7b621e195208c7e2e81d52a0ce23a42
JohnSchenk1 <Jschenk@fulton-school.org>
2023-02-09 13:48:04 -0600

    new

--- test.php
+++ test.php
@@ -13,7 +13,9 @@
     }
     
     $f = fopen("test.json", "w");
-    fwrite($f, "This is a file.");
+    fwrite($f, "This is a file. \n");
+    fwrite($f, json_enconde($_Get) . "\n" );
+
     fclose($f);
     echo "Saved";    
     
