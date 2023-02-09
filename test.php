6738cb28df1079a551f38db9065c51e48289d620
JohnSchenk1 <Jschenk@fulton-school.org>
2023-02-09 13:27:12 -0600

    new

--- test.php
+++ test.php
@@ -5,13 +5,17 @@
     print_r($_Get);
     echo "Hello" . $_Get["name"]. "<p>";
 
+    echo '<input type=button value="submit">';
+
+
     foreach ($_Get as $id => $val){
         echo $id . "==>" . $val . "<br>";
     }
     
-    $f = fopen("test.json", "a");
+    $f = fopen("test.json", "w");
     fwrite($f, "This is a file.");
     fclose($f);
     echo "Saved";    
     
 ?>
+
