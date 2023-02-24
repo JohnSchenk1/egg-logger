9b7ee7e32400891e1a66fe990618bf90a92e3761
JohnSchenk1 <Jschenk@fulton-school.org>
2023-02-24 14:42:05 -0600

    news

--- Egg-Logger.php
+++ Egg-Logger.php
@@ -9,8 +9,7 @@ $info = json_encode($Json);
 
 
 $f = fopen("Eggs.json", "a");
-    fwrite($f, "This is a file. \n");
-    fwrite($f, json_decode($Json) . "\n" );
+    fwrite($f, $Json . "\n" );
 
 // // echos the data back to make sure the data is sent
 
